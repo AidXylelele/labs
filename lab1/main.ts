d5bb42c9998d9d73f63a4214dc7130d0b6fe3621
Vladisalv <warboyka48@gmail.com>
2023-02-26 21:40:06 +0200

    refactored: Fixed some bugs
     Added process.exit() in main file

--- lab1/main.ts
+++ lab1/main.ts
@@ -4,7 +4,8 @@ import { nonInteractiveApp } from './src/non-interactive';
 const FILENAME_POSITION = 3;
 
 (async () => {
-    process.argv.length === FILENAME_POSITION
-      ? nonInteractiveApp.start(process.argv[2])
-      : await interactiveApp.start();
+  process.argv.length === FILENAME_POSITION
+    ? nonInteractiveApp.start(process.argv[2])
+    : await interactiveApp.start();
+  return process.exit();
 })();
