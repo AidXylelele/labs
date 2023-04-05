4199a97e4f5e6c95fb9e19e6410c0fbbaa69746e
AIDXYLELELE <warboyka48@gmail.com>
2023-04-05 05:43:09 +0200

    Added matrix endpoint
    Added some solution to the task with matrix endpoint

--- lab3/python_task/spaceship/routers/api.py
+++ lab3/python_task/spaceship/routers/api.py
@@ -6,3 +6,25 @@ router = APIRouter()
 @router.get('')
 def hello_world() -> dict:
     return {'msg': 'Hello, World!'}
+
+@router.get('/matrix')
+def matrix() -> dict:
+    firstRows = 10
+    firstCol = 10
+    secondRows = 10
+    secondCol = 10
+    if firstCol != secondRows:
+        response = {
+            'result': 'Error!'
+        }
+    else:
+        first_matrix = numpy.random.rand(firstRows, firstCol)
+        second_matrix = numpy.random.rand(secondRows, secondCol)
+        result = numpy.dot(first_matrix, second_matrix)
+        response = {
+            "matrix_a": first_matrix.tolist(),
+            "matrix_b": second_matrix.tolist(),
+            "product": result.tolist()
+        }
+
+    return response
